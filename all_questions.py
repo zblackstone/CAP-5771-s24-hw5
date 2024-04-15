ffe5d699d2e385fd6585fe8aa7d7c7376e4aedb3
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-04-15 00:04:29 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -150,24 +150,24 @@ def question7():
 
     # type: string
     # choices: ['C1', 'C2', 'None']
-    answers['(i) Best classifier?'] = None
+    answers['(i) Best classifier?'] = 'C2'
 
     # type: explain_string
-    answers['(i) Best classifier, explain'] = None
+    answers['(i) Best classifier, explain'] = 'Both classifiers have the same precision but C2 has a higher Recall/TPR, F-1 Measure at the cost of a high FPR. This shows that C2 is better at identifying positives and false positives are not detrimental. If false positives pose a risk then C1 would be better.'
 
     # type: string
     # choices: ['TPR-FPR', 'precision-recall-F1-Measure']
-    answers['(ii) appropriate metric pair'] = None
+    answers['(ii) appropriate metric pair'] = 'precision-recall-F1-Measure'
 
     # type: explain_string
-    answers['(ii) appropriate metric pair, explain'] = None
+    answers['(ii) appropriate metric pair, explain'] = 'Precision, Recall, F-1 Measure provides a more in depth view of performance as it shows a proportional comparision of the two classifiers.'
 
     # type: string
     # choices: ['C1', 'C2', 'C3']
-    answers['(iii) preferred classifier?'] = None
+    answers['(iii) preferred classifier?'] = 'C3'
 
     # type: explain_string
-    answers['(iii) best classifier, explain'] = None
+    answers['(iii) best classifier, explain'] = 'C3 is essentially a better C1. It has a higher precision (higher than C2 as well), F-1 measure and Recall, while still keeping the low FPR of C1. While these metrics are not as good as C2, the tradeoff of such a high FPR in my opinion is worth it.' 
     return answers
 
 
@@ -176,23 +176,23 @@ def question8():
     answers = {}
 
     # type: eval_float
-    answers['(a) precision for C0'] = None
+    answers['(a) precision for C0'] = 0.1
 
     # type: eval_float
-    answers['(a) recall for C0'] = None
+    answers['(a) recall for C0'] = 0.1
 
     # type: eval_float
-    answers['(b) F-measure of C0'] = None
+    answers['(b) F-measure of C0'] = 
 
     # type: string
     # choices: ['yes', 'no', 'unknown']
-    answers['C1 better than random?'] = None
+    answers['C1 better than random?'] = 'no'
 
     # type: float
     # What is the range of p for which C1 is better than random?  What is
     # "?" in the expression "p > ?"
 
-    answers['p-range'] = None
+    answers['p-range'] = 0.3
     return answers
 
 
@@ -202,18 +202,22 @@ def question9():
 
     # type: dict[string,float]
     # keys: ['recall', 'precision', 'F-measure', 'accuracy']
-    answers['(i) metrics'] = None
+    answers['(i) metrics'] = {
+        'recall': 0.533,
+        'precision': 0.615,
+        'F-measure': 0.571,
+        'accuracy': 0.88
+    }
+    # type: string
+    # choices: ['recall', 'precision', 'F-measure', 'accuracy']
+    answers['(i) best metric?'] = 'F-measure'
 
     # type: string
     # choices: ['recall', 'precision', 'F-measure', 'accuracy']
-    answers['(i) best metric?'] = None
-
-    # type: string
-    # choices: ['recall', 'precision', 'F-measure', 'accuracy']
-    answers['(i) worst metric?'] = None
+    answers['(i) worst metric?'] = 'accuracy'
 
     # type: explain_string
-    answers['(ii) Explain your choices of best and worst metrics'] = None
+    answers['(ii) Explain your choices of best and worst metrics'] = 'F-measure combines precision and recall in a single statistic, making it the clear choice for best metric between the three. Due to imbalanced classes, accuracy can be skewed heavily as it only accounts for the total number of mistakes rather than the type of mistake (doesnt account for the discrepency between FP and FN in context).
     return answers
 
 
@@ -223,21 +227,21 @@ def question10():
 
     # type: string
     # choices: ['T1', 'T2']
-    answers['(a) better test based on F-measure?'] = None
+    answers['(a) better test based on F-measure?'] = 'T1'
 
     # type: string
     # choices: ['T1', 'T2']
-    answers['(b) better test based on TPR/FPR?'] = None
+    answers['(b) better test based on TPR/FPR?'] = 'T2'
 
     # type: string
     # choices: ['F1', 'TPR/FPR']
-    answers['(c) Which evaluation measure to use between the two tests?'] = None
+    answers['(c) Which evaluation measure to use between the two tests?'] = 'TPR/FPR'
+    # type: explain_string
+    answers['(c) Which evaluation measure? Explain'] = 'Given the context of the situation, failing to diagnose someone with cancer seems like a bigger deal than falsely diagnosing someone. Therefore, TPR/FPR is the most appropriate metric to be used.'
+
 
     # type: explain_string
-    answers['(c) Which evaluation measure? Explain'] = None
-
-    # type: explain_string
-    answers['(d) Example scenario where you would reverse choise in (c)'] = None
+    answers['(d) Example scenario where you would reverse choise in (c)'] = 'Email-spam detection would be an appropriate situation to use F-measure rather than TPR/FPR as FP and FN are less dire and it does not end someones life if they get a spam email.'
     return answers
 #-----------------------------------------------------------
 if __name__ == '__main__':
