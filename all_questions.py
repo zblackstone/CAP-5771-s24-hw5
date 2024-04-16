4d603d3b47d4dd7d0fe2aedb35d3391d619081e6
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-04-15 21:07:18 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -18,7 +18,7 @@ def question1():
 
     # type: float
     # Calculate the probability.
-    answers['(c)'] = 0.08
+    answers['(c)'] = 0.008
     return answers
 
 
@@ -54,7 +54,7 @@ def question2():
     # The formulas should only use the variable 'p'. The formulas should be
     # a valid Python expression. Use the functions in the math module as
     # required.
-    answers['(c) Weight update'] = '0.5 * math.log((1-p)/(p))'
+    answers['(c) Weight update'] = "0.5 * math.log((1-p)/(p))"
 
     # type: float
     # the answer should be correct to 3 significant digits
