f8d136251b224cedacdafb0f3b0cc53aa7c1ac36
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-04-15 21:29:43 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -1,7 +1,6 @@
 import pytest
 from all_questions import *
 import pickle
-import math
 
 
 #-----------------------------------------------------------
@@ -54,7 +53,7 @@ def question2():
     # The formulas should only use the variable 'p'. The formulas should be
     # a valid Python expression. Use the functions in the math module as
     # required.
-    answers['(c) Weight update'] = "0.5 * math.log((1-p)/p)"
+    answers['(c) Weight update'] = '0.5 * math.log((1 - p) / p)'
 
     # type: float
     # the answer should be correct to 3 significant digits
