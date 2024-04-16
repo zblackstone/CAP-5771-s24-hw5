89b226a27f28ac3756c01f0d520fa3fdea1158bd
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-04-15 21:03:25 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -54,7 +54,7 @@ def question2():
     # The formulas should only use the variable 'p'. The formulas should be
     # a valid Python expression. Use the functions in the math module as
     # required.
-    answers['(c) Weight update'] = 0.424
+    answers['(c) Weight update'] = '0.5 * math.log((1-p)/(p))'
 
     # type: float
     # the answer should be correct to 3 significant digits
